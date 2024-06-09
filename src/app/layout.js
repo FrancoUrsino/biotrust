a03ac0e332fe4be8e922bd143527a1fcc6530176
Franco Ursino <franncoursinno@gmail.com>
2024-06-09 00:15:42 -0300

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,11 @@ import { Libre_Franklin } from "next/font/google";
 const font = Libre_Franklin({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "BioTrust",
-  description: "",
+title: "Biotrust",
+  description: "Nuestra empresa es el principal proveedor de equipamiento médico, ofrecemos la mas avanzada tecnología, procurando estar actualizados y de acuerdo a las normas vigentes.",
+  keywords: ['Biotrust', 'certificaciones', 'medicina', 'alquiler de artefactos medicos', 'electromedicina', 'equipamiento medico', 'normas vigentes'],
+  authors: [{ name: 'Marina' }, { name: 'Franco Ursino', url: 'https://francoursino-portfolio.vercel.app/' }],
+  creator: 'Franco Ursino',
   icons: {
     icon: ['/favicon.ico?v=4'],
   },
